b8e73382e1a53ceb4588aaab03542a4eb8e5d937
ezgi02 <104259592+ezgi02@users.noreply.github.com>
2023-11-01 16:34:09 +0300

    Add files via upload

---
+++ MetinGir/MetinGir/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir metin girin:");
+        string metin = Console.ReadLine();
+
+        bool sonuc = SessizVarMi(metin);
+        Console.WriteLine(sonuc);
+    }
+
+    static bool SessizVarMi(string metin)
+    {
+        for (int i = 0; i < metin.Length - 1; i++)
+        {
+            char mevcutKarakter = char.ToLower(metin[i]);
+            char sonrakiKarakter = char.ToLower(metin[i + 1]);
+
+            if (Char.IsLetter(mevcutKarakter) && Char.IsLetter(sonrakiKarakter))
+            {
+                if ("aeiou".Contains(mevcutKarakter) && "aeiou".Contains(sonrakiKarakter))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
